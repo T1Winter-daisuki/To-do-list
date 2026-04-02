0c6522ea196b88485d3771f6d9cb8dcac8662627
T1Winter-daisuki <mocchaneo@gmail.com>
2026-04-02 14:47:53 +0700

    Fix delay web BE

--- be/src/1_config/db.js
+++ be/src/1_config/db.js
@@ -9,14 +9,18 @@ const config = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    max: 10, // Tối đa 10 kết nối cùng lúc, k nên cao quá với sv free
+    idleTimeoutMillis: 30000, // Đóng kết nối nếu không hoạt động trong 30 giây (giúp dọn rác khi máy Sleep)
+    connectionTimeoutMillis: 5000, // Chỉ đợi 5s khi tạo kết nối mới, quá hạn là báo lỗi
+    keepAlive: true,
+    keepAliveInitialDelayMillis: 10000
 };
 
 const pool = new Pool(config);
 
 pool.on('error', (err) => {
     console.error('Unexpected Database Connection Error:', err);
-    process.exit(-1);
 });
 
 export default pool;
